1f989d355fa9493f297cab035660446c90d129a9
Justin Lund <47057520+Justin-Lund@users.noreply.github.com>
2020-02-25 11:32:06 -0800

    v1,0 > v2.0
    The script now keeps track of all of the times that the user has been locked out

--- Keep-Account-Unlocked.ps1
+++ Keep-Account-Unlocked.ps1
@@ -1,11 +1,11 @@
 ######################################################### 
-#               Account Unlock Tool V1.0                # 
+#               Account Unlock Tool V2.0                # 
 #                Created By: Justin Lund                # 
 #             https://github.com/Justin-Lund/           # 
 ######################################################### 
 
 
-#--------------Functions--------------#
+#--------------User Verification--------------#
 
 # Define function to verify user is valid
 Function Test-User {
@@ -27,33 +27,6 @@ Function Test-User {
     }
 }
 
-# Define function to unlock the user
-Function Unlock-User {
-    
-    # Store status of whether or not user is locked out in a new variable
-    $LockedOutStatus = (Get-ADUser $Username -Properties LockedOut).LockedOut
-
-    If ($LockedOutStatus -eq $True)
-        {
-            Write-Host "`n$Username is locked out. Unlocking account" -ForegroundColor Red
-
-            # Unlock account
-            Unlock-ADAccount -Identity $Username
-
-            $CurrentTime = Get-Date
-            Write-Host "User unlocked at $CurrentTime`n" -ForegroundColor Cyan
-            Sleep 10
-        }
-
-    # Otherwise, pause
-    Else
-        {
-            Write-Host "$Username is not currently locked out."
-            Sleep 5
-        }
-
-    }
-
 
 #--------------Start Main--------------#
 
@@ -67,15 +40,54 @@ Test-User
 # Infinite Loop
 While ($True) {
 
-    # Loop 25 times, then clear screen
-    While ($Counter -lt 25)
+    # Loop 20 times, then clear screen
+    While ($Counter -lt 20)
         {
-            Unlock-User
+
+            # Store status of whether or not user is locked out in a new variable
+            $LockedOutStatus = (Get-ADUser $Username -Properties LockedOut).LockedOut
+
+            # If the user is locked out
+            If ($LockedOutStatus -eq $True) {
+                    Write-Host "`n$Username is locked out. Unlocking account" -ForegroundColor Red
+
+                    # Unlock account
+                    Unlock-ADAccount -Identity $Username
+
+                    $CurrentTime = Get-Date
+                    Write-Host "User unlocked at $CurrentTime`n" -ForegroundColor Cyan
+
+                    $LockoutRecord += "$CurrentTime `n"
+                    $LockoutCount += 1
+                    $LockedOutAtAll = $True
+            
+                    Sleep 10
+            }
+
+            # If the user is not locked out
+            Else
+                {
+                    Write-Host "$Username is not currently locked out."
+                    Sleep 5
+            }
 
             # Increase counter
             $Counter++
         }
+
+    # If user has been locked out at all, print the times the lockouts have occurred at after clearing the screen
     Clear-Host
+
+    If ($LockedOutAtAll -eq $True) {
+        If ($LockoutCount -eq 1) {
+            Write-Host "User has been locked out 1 time at:" -ForegroundColor Red
+            Write-Host $LockoutRecord -ForegroundColor Cyan
+        }
+        Else {
+            Write-Host "User has been locked out $LockoutCount times at:" -ForegroundColor Red
+            Write-Host $LockoutRecord -ForegroundColor Cyan
+            }
+    }
 
     # Reset counter
     $Counter = 0
